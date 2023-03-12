424025dc473f708535f74df84c2eeedfd5741049
庞 <3096470072@qq.com>
2023-03-12 18:47:05 +0800

    fix: 🐛正逆解相互验证通过、逆解实机验证通过

--- model/mecanum.c
+++ model/mecanum.c
@@ -10,13 +10,13 @@
 
 //运动学正解
 void mecanum_positive_calculate(const mecanum_constant_t* model,mecanum_input_t* source,mecanum_output_t* results){
-    float v13 = (*source)[0] + (*source)[2];
-    float v24 = (*source)[1] + (*source)[3];
+    float v23 = (*source)[1] + (*source)[2];
+    float v14 = (*source)[0] + (*source)[3];
     float rxy = 1.0f / (model->x_len + model->y_len);
     float r4 = model->wheel_r / 4.0f;
-    results->x_speed = (v13 + v24) * r4;
-    results->y_speed = (v24 - v13) * r4;
-    results->cr_speed = ( (*source)[3] + (*source)[2] - (*source)[0] - (*source)[1] ) * rxy * r4;
+    results->x_speed = (v23 + v14) * r4;
+    results->y_speed = (v14 - v23 ) * r4;
+    results->cr_speed = ( (*source)[0] + (*source)[2] - (*source)[1] - (*source)[3] ) * rxy * r4;
 }
 
 //运动学逆解
@@ -31,9 +31,7 @@ void mecanum_inverse_calculate(const mecanum_constant_t* model,mecanum_input_t* 
     }
     (*source)[0] = vz + w ;
     (*source)[1] = vf - w ;
-    (*source)[2] = vz + w ;
-    (*source)[3] = vf - w ;
+    (*source)[2] = vf + w ;
+    (*source)[3] = vz - w ;
 }
-
-
 
